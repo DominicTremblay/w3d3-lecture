31521b97715b7abb5ee52fd0b5ec15e25ac9154c
Dominic Tremblay <dominictremblay24@gmail.com>
2019-01-22 15:42:28 -0500

    added the ajax call to get the articles

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
   const ROOT_URL = 'http://jsonplaceholder.typicode.com';
+
+  // load all articles via AJAX request, then render them on page
+  // gets 100 articles at a time
+  const loadArticles = () => {
+    const options = {
+      method: 'GET',
+      url: `${ROOT_URL}/posts`,
+    };
+    $.ajax(options)
+      .done(articles => {
+        console.log(articles);
+      })
+      .fail(err => console.log('Error: ', err))
+      .always(() => {
+        console.log('Request completed.');
+      });
+  };
+
+  loadArticles();
 }); // End Doc Ready
